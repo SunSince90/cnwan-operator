0f80fb6ca1c078b4cf8f19ff5b7ee068c8166926
Elis Lulja <elulja@cisco.com>
2021-02-22 12:03:57 +0100

    Update main to start controller utils (#37)
    This commit updates main.go to reflect the changes in the controller.
    More precisely, main starts controller utils and sets it up in the
    namespace and service controllers.
    
    Signed-off-by: Elis Lulja <elulja@cisco.com>

--- main.go
+++ main.go
@@ -87,6 +87,11 @@ func main() {
 	}
 	viper.Set(types.AllowedAnnotationsMap, allowedAnnotations)
 
+	ctrlUtils := &controllers.Utils{
+		AllowedAnnotations: viper.GetStringSlice(types.AllowedAnnotations),
+		CurrentNsPolicy:    types.ListPolicy(viper.GetString(types.NamespaceListPolicy)),
+	}
+
 	// Create a handler for gcp service directory
 	credsPath := "./credentials/gcloud-credentials.json"
 	sdHandler, err := sd.NewHandler(ctx, viper.GetString(types.SDProject), viper.GetString(types.SDDefaultRegion), credsPath, 30)
@@ -120,6 +125,7 @@ func main() {
 		Log:           ctrl.Log.WithName("controllers").WithName("Service"),
 		Scheme:        mgr.GetScheme(),
 		ServRegBroker: srBroker,
+		Utils:         ctrlUtils,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Service")
 		os.Exit(1)
@@ -129,6 +135,7 @@ func main() {
 		Log:           ctrl.Log.WithName("controllers").WithName("Namespace"),
 		Scheme:        mgr.GetScheme(),
 		ServRegBroker: srBroker,
+		Utils:         ctrlUtils,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Namespace")
 		os.Exit(1)
