13d5eb6de1dd9cdc9f8fc0fd78484e859d869994
Elis Lulja <elulja@cisco.com>
2021-02-23 14:47:38 +0100

    Create EndpointSliceReconciler
    This commit adds the EndpointSliceReconciler and makes it inherit
    the BaseReconciler.
    
    Signed-off-by: Elis Lulja <elulja@cisco.com>

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright © 2020 Cisco
+// Copyright © 2020, 2021 Cisco
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -34,6 +34,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	"github.com/CloudNativeSDWAN/cnwan-operator/controllers"
+	discoveryv1beta1 "k8s.io/api/discovery/v1beta1"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -51,6 +52,7 @@ func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
 	_ = corev1.AddToScheme(scheme)
+	_ = discoveryv1beta1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -130,6 +132,11 @@ func main() {
 	}
 	if err = base.NamespaceReconciler().SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Namespace")
+		os.Exit(1)
+	}
+	// TODO: enable me only if settings enable this feature
+	if err = base.EndpointSliceReconciler().SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "EndpointSlice")
 		os.Exit(1)
 	}
 	// +kubebuilder:scaffold:builder
